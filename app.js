3bbc4c32a9080ec37cf0e91c9b67d34d6212995f
franciscofrederico124-tech <franciscofrederico124@gmail.com>
2026-05-12 12:03:48 +0100

    V4

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors")
 const { engine } = require("express-handlebars");
 
 const app = express();
 
+app.use(cors({
+  origin: "*"
+}))
 app.use(express.json());
 
 let systemBulb = {
